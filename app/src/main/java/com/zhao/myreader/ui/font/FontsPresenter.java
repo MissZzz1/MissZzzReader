b2d37a8fa95691a1341d0fdae79f4ce9c5cadf10
zhao <357562448@qq.com>
2017-08-07 17:28:14 +0800

    新增字体选择

--- app/src/main/java/com/zhao/myreader/ui/font/FontsPresenter.java
+++ app/src/main/java/com/zhao/myreader/ui/font/FontsPresenter.java
@@ -33,8 +33,8 @@ public class FontsPresenter implements BasePresenter {
     };
 
     public FontsPresenter(FontsActivity fontsActivity) {
-        mFontsActivity = fontsActivity;
 
+        mFontsActivity = fontsActivity;
     }
 
 
